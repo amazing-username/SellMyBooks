dca32a6cf4a9013cd70ba1d7184b861bc3b0d161
amazing-username <kdeng02@aurora.edu>
2016-10-15 22:47:29 -0500

    Issue #1 route set up for html

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var app = express();
+//var mongo = require('mongo');
+//var mongojs = mongo();
 
-app.use(express.static(__dirname + "public"));
+app.use(express.static(__dirname + '/public'));
 
 app.listen(3000);
 console.log('Server runnong port 3000');
