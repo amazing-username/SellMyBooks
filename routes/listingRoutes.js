1d080f8a86beae19d5198246c1e4ac80d5e17dd2
InjectedSoul <xshiroppu@gmail.com>
2016-11-09 10:24:47 +0000

    Added an get route for listingRoutes.js. It grabs all books.

--- routes/listingRoutes.js
+++ routes/listingRoutes.js
@@ -3,25 +3,35 @@ var router = express.Router();
 var mongoose = require('mongoose');
 var Listing = require('../models/listings.js');
 
-router.route('/listings').post(function(req, res) {
-	var listing = new Listing();
-	listing.title = req.body.title;
-	listing.author = req.body.author;
-	listing.isbn = req.body.isbn;
-	listing.cost = req.body.cost;
-	listing.stat = req.body.stat;
+router.route('/listings')
 
-	listing.save(function(err) {
-		if (err) 
-			res.send(err);
+	.post(function(req, res) {
+	
+		var listing = new Listing();
+		listing.title = req.body.title;
+		listing.author = req.body.author;
+		listing.isbn = req.body.isbn;
+		listing.cost = req.body.cost;
+		listing.stat = req.body.stat;
+
+		listing.save(function(err) {
+			if (err) 
+				res.send(err);
 		 
-		res.json({message: 'Created Listing'});
-		
-	});
+			res.json({message: 'Created Listing'});
+	
+		});
 });
 
-router.get('/deleteListing', function(req, res, next) {
-	res.send('deleting a listing');
+router.route('/listings/get')
+
+	.get(function(req, res) {
+		Listing.find(function(err, listings) {
+			if (err)
+				res.send(err);
+			
+			res.json(listings);
+		});
 });
 
 module.exports = router;
