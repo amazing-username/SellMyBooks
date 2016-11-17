1e91e1a24dbf70f0c091ce91171a057a79890118
amazing-username <kdeng02@aurora.edu>
2016-11-17 20:43:52 +0000

    create user route partially works. The update, get by id, and delete do not work or partially work

--- models/users.js
+++ models/users.js
@@ -1,27 +1,11 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 var UsersSchema = new Schema({
-  username: {
-    type: String,
-    required: true
-  },
-  password: {
-    type: String,
-    required: true
-  }
-  firstname: {
-    type: String,
-    required: true
-  }	  
-  lastname: {
-    type: String,
-    required: true
-  }	  
-  emailaddress: {
-    type: String,
-    required: true
-    unique: true;	  
-  }	  
+  username: String,
+  password: String,
+  firstname: String,
+  lastname: String,
+  emailaddress: String,
 	
 });
 
